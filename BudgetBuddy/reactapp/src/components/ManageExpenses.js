5387b9cd76f3cffcef50fc4dfd9ef1a8aaf36740
Vaibhav Jogad <vmjogad37@gmail.com>
2023-08-23 19:56:18 +0530

    Update UI

---
+++ BudgetBuddy/reactapp/src/components/ManageExpenses.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Budget from "./Budget";
+
+export default function ManageExpenses()
+{
+    return(
+        <div>
+            <Budget></Budget>
+        </div>
+    )
+}
